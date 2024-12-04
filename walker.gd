72c178c9d40e18cada435267791697ead0514823
dionebigode <jamarson@gmail.com>
2024-12-04 20:04:56 -0300

    Implement a regular timer based on get_process_frames

--- walker.gd
+++ walker.gd
@@ -3,16 +3,12 @@ extends Sprite2D
 var screen_size := DisplayServer.window_get_size() as Vector2i
 var rng := RandomNumberGenerator.new() as RandomNumberGenerator
 
-	
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.position = screen_size / 2.0 
-	var my_random_number = rng.randi_range(-10, 10)
-	print(my_random_number)
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
 func _process(delta: float) -> void:
-	pass
+	#how to make something execute at regular frames, probably not a good idea for a commercial game
+	#https://docs.godotengine.org/en/stable/classes/class_engine.html#class-engine-method-get-process-frames
+	if Engine.get_process_frames() % 10 == 0:
+		var choice := rng.randi_range(0, 3) as int #returns 0, 1, 2, 3		
+		print(choice)
