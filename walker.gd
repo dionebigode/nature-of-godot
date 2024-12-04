0d2ac909b09dbcd1cc6793394b2726f58588b5cc
dionebigode <jamarson@gmail.com>
2024-12-04 17:38:17 -0300

    First step of the book is to spawn the icon on the middle of screen Since I want to be literal in my implementation, first we need to get the width of the game screen in Godot
    This can be accomplished by using ´DisplayServer.window_get_size()´
    https://docs.godotengine.org/en/stable/classes/class_displayserver.html#class-displayserver-method-screen-get-size
    and it returns a Vector2i, which we will pass to the position.
    
    It's also possible to get the wrong size if you use `screen_get_size`
    instead of `windows_get_size`

---
+++ walker.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+var screen_size := DisplayServer.window_get_size() as Vector2i
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print(screen_size)
+	print(screen_size/2.0)
+	position = screen_size / 2.0 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
