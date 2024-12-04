9d72d96c6960ea8a9f6a0c03c0655c917af6ebec
dionebigode <jamarson@gmail.com>
2024-12-04 19:58:38 -0300

    Implementing a random number generator

--- walker.gd
+++ walker.gd
@@ -1,10 +1,16 @@
 extends Sprite2D
 
 var screen_size := DisplayServer.window_get_size() as Vector2i
+var rng := RandomNumberGenerator.new() as RandomNumberGenerator
+
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	position = screen_size / 2.0 
+	self.position = screen_size / 2.0 
+	var my_random_number = rng.randi_range(-10, 10)
+	print(my_random_number)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
